5653362c4bee14eaebe1cbd5620f66b884656d3f
BrunoFelipi <bruno.souza@senior.com.br>
2017-01-25 15:37:47 -0200

    Iniciado Home

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,6 +1,15 @@
 app.controller('homeCtrl',function($scope, CalendarioService, ComunicadosService, EstatutoService, EventosService, FinancasService, JogosService
     , PromocoesService, toastr, $location, $filter){
 
+    $scope.comunicados;
+
+    var promise = ComunicadosService.getComunicados();
+    promise.then(function (response) {
+        $scope.comunicados = response.data;
+    }, function (error) {
+        Materialize.toast('Erro de conexão com o<br>servidor', 4000);
+    });
+
     $scope.enviarEstatuto = function(){
         console.log($scope.concordo);
     }
---
+++ controllers/testeCtrl.js
@@ -0,0 +1,5 @@
+app.controller('testeCtrl',function($scope, CalendarioService, toastr, $location, $filter){
+
+    
+
+});
